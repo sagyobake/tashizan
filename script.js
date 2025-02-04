a44f859034aed75ef0bb062c919d7d538454b7cb
kotaro fujishima <sagyobake@localhost.localdomain>
2025-02-04 20:39:08 +0900

    upgrade

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let correct_array = [];
 let check = 0; //縦、横の列に重複がない場合に加算されていく。
 let total_array = []; //縦、横の列の合計値を持つ配列
 let tai = 0; //total_array専用のインデックス
+let answer_obj = {}; //ユーザの解答を入れる配列
+let answer_array = []; //answer_objをソートするために用いる配列
 
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
@@ -29,7 +31,7 @@ const whCheck = (array) => {
 const correctCheck = () => {
     let array = [];
     for (let i = 0; i < 4; i++) {
-        const r = getRandomIntInclusive(1, 9);
+        const r = getRandomIntInclusive(1, 6);
         array.push(r);
     }
     check = 0;
@@ -51,17 +53,18 @@ const correctCheck = () => {
 
     //console.log(check);
     if (check === 4) {
+        console.log(array); //正解となる配列を出力  
         return array;
     } else {
         return correctCheck(array);
     }
 }
 
-correctCheck();
+correct_array = correctCheck();
 
-const blockGenerator = (class_value, id_value, text_value) => {
+const blockGenerator = (class_value, id_value, text_value, tag) => {
     const main = document.getElementById('main');
-    const block = document.createElement('div');
+    const block = document.createElement(tag);
     block.setAttribute('class', class_value);
     block.setAttribute('id', id_value);
     block.innerText = text_value;
@@ -71,16 +74,73 @@ const blockGenerator = (class_value, id_value, text_value) => {
 for (let i = 0; i < 9; i++) {
     if (i < 3 || i % 3 === 0) {
         if (i === 0) {
-            blockGenerator('hint_block', `id${i}`, '');
+            blockGenerator('hint_block', `id${i}`, '', 'div');
         } else {
-            blockGenerator('hint_block', `id${i}`, total_array[tai]);
+            blockGenerator('hint_block', `id${i}`, total_array[tai], 'div');
             tai++;
         }
     } else {
-        blockGenerator('input_block', `id${i}`, '');
+        blockGenerator('input_block', `id_${i}`, '', 'div');
     }
     if (i === 8) {
         tai = 0;
     }
 }
+for (let i = 0; i < 9; i++) {
+    blockGenerator('button', `b_${i}`, i + 1, 'button');
+}
+
+const close = document.getElementById('close');
+const popWin = document.getElementById('popWin');
+
+close.addEventListener('click', () => {
+    popWin.style.display = 'none';
+});
+
+const checkingAnswers = () => {
+    let sorted_array = []; //ユーザーの解答を正しい順序に並び替えたもの
+    console.log(answer_obj);
+    answer_array = Object.keys(answer_obj);
+    const array = answer_array.sort();
+
+    array.forEach(element => {
+        const value = Number(answer_obj[element]);
+        console.log(value);
+        sorted_array.push(value);
+    });
+
+    console.log(JSON.stringify(correct_array));
+    console.log(JSON.stringify(sorted_array));
+    if (JSON.stringify(sorted_array) === JSON.stringify(correct_array)) {
+        console.log('正解！');
+        popWin.style.display = 'flex';
+    }
+}
+
+let clicked_id = '';
+let input_check = [];
+document.addEventListener('click', (e) => {
+    const element = e.target.className;
+    const id = e.target.id;
+    //console.log(element);
+    if (element === 'button') {
+        if (clicked_id !== '') {
+            const button = document.getElementById(id).innerText;
+            console.log(button);
+            const input = document.getElementById(clicked_id);
+            input.innerText = button;
+            answer_obj[clicked_id] = button;
+            checkingAnswers();
+        }
+    }
+    if (element === 'input_block') {
+        clicked_id = id;
+
+        input_check.push(id);
+        const last_block = document.getElementById(input_check.slice(-2)[0]);
+        last_block.style.border = '1px solid';
+        const block = document.getElementById(id);
+        block.style.border = '3px solid';
+    }
+});
 
